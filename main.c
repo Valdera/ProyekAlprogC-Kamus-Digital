d449e6d657d121e0b38ef4022107532da941073b
Fauzan Valdera <f.valdera@yahoo.co.id>
2021-10-28 18:26:02 +0700

    kelar

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 typedef struct Item {
     char* word;
     char* description;
+    char* uncommon;
 } Item;
 
 typedef struct Kamus {
@@ -16,6 +17,7 @@ typedef struct Kamus {
     int length;
 } Kamus;
 
+void menu(Kamus* kamus);
 void display_item(Item item);
 void display_all(Kamus* kamus);
 void write_data(Kamus* kamus);
@@ -27,8 +29,27 @@ void searchWord(Kamus* kamus);
 void swapItem(Item* item_1, Item* item_2);
 Kamus initialize();
 
+/**
+ * Main function
+ */
 int main() {
+    // Initialize kamus
     Kamus kamus = initialize();
+
+    // Run the menu function
+    menu(&kamus);
+
+    // Free allocated heap memory
+    free(kamus.data);
+
+    return 0;
+}
+
+/**
+ * Function to run menu program
+ * @param Kamus* kamus
+ */
+void menu(Kamus* kamus) {
     int loop = 1;
     int menu;
     while (loop) {
@@ -43,21 +64,21 @@ int main() {
         scanf("%d", &menu);
         switch (menu) {
             case 1:
-                display_all(&kamus);
+                display_all(kamus);
                 break;
             case 2:
-                add_item(&kamus);
+                add_item(kamus);
                 break;
             case 3:
-                sortAscending(&kamus);
-                display_all(&kamus);
+                sortAscending(kamus);
+                display_all(kamus);
                 break;
             case 4:
-                sortDescending(&kamus);
-                display_all(&kamus);
+                sortDescending(kamus);
+                display_all(kamus);
                 break;
             case 5:
-                searchWord(&kamus);
+                searchWord(kamus);
                 break;
             default:
                 loop = 0;
@@ -65,17 +86,24 @@ int main() {
         }
         printf("\n");
     }
-    free(kamus.data);
-    return 0;
 }
 
+/**
+ * Function to display word in dictionary
+ * @param Item item
+ */
 void display_item(Item item) {
     printf("----------------------");
-    printf("\nKata      : %s", item.word);
-    printf("\nDeskripsi : %s", item.description);
+    printf("\nKata            : %s", item.word);
+    printf("\nBentuk tak baku : %s", item.uncommon);
+    printf("\nDeskripsi       : %s", item.description);
     printf("----------------------");
 }
 
+/**
+ * Function to display all words in dictionary
+ * @param Kamus* kamus
+ */
 void display_all(Kamus* kamus) {
     int i;
     printf("\n-------LIST OF WORDS-------\n");
@@ -85,10 +113,26 @@ void display_all(Kamus* kamus) {
     }
 }
 
+/**
+ * Function to refresh the state of dictionary
+ * @param Kamus* kamus
+ */
+void refresh(Kamus* kamus) {
+    free(kamus->data);
+    kamus->data = (Item*)malloc(KAMUS_SIZE * sizeof(Item));
+    kamus->length = 0;
+    read_data(kamus);
+}
+
+/**
+ * Function to save the dictionary to .txt
+ * @param Kamus* kamus
+ */
 void write_data(Kamus* kamus) {
     FILE* fp = fopen(FILE_NAME, "w");
     int i;
 
+    // If file not found
     if (fp == NULL) {
         printf("Error opening the file %s", FILE_NAME);
         return;
@@ -96,20 +140,31 @@ void write_data(Kamus* kamus) {
 
     for (i = 0; i < kamus->length; i++) {
         char* text = (char*)malloc((MAX_SIZE_DESC + MAX_SIZE_WORD) * sizeof(char));
+        // Insert the word
         strcpy(text, kamus->data[i].word);
         strcat(text, "-");
+        // Insert uncommon form of the word
+        strcat(text, kamus->data[i].uncommon);
+        strcat(text, "-");
+        // Insert description of the word
         strcat(text, kamus->data[i].description);
         fprintf(fp, text);
     }
     fprintf(fp, "\n");
 
+    // Close the file
     fclose(fp);
 }
 
+/**
+ * Function to read the saved dictionary in .txt
+ * @param Kamus* kamus
+ */
 void read_data(Kamus* kamus) {
     FILE* fp = fopen(FILE_NAME, "r");
     char line[256];
 
+    // If file not found
     if (fp == NULL) {
         printf("Error opening the file %s", FILE_NAME);
         return;
@@ -117,50 +172,75 @@ void read_data(Kamus* kamus) {
 
     while (fgets(line, sizeof(line), fp)) {
         Item item;
-        char *word, *description;
+        char *word, *uncommon, *description;
+
+        // Insert the word
         item.word = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
         word = strtok(line, "-");
         strcpy(item.word, word);
+
+        // Insert uncommon form of the word
+        item.uncommon = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
+        uncommon = strtok(NULL, "-");
+        strcpy(item.uncommon, uncommon);
+
+        // Insert description of the word
         item.description = (char*)malloc(MAX_SIZE_DESC * sizeof(char));
         description = strtok(NULL, "-");
         strcpy(item.description, description);
+
         kamus->data[kamus->length++] = item;
     }
 
+    // Close the file
     fclose(fp);
 }
 
+/**
+ * Function to add item to dictionary
+ * @param Kamus* kamus
+ */
 void add_item(Kamus* kamus) {
     Item item;
+
+    // Get the word
     item.word = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
     printf("Masukkan suatu kata : ");
     fflush(stdin);
     gets(item.word);
+
+    // Get the uncommon form of the word
+    item.uncommon = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
+    printf("Masukkan bentuk tidak baku dari %s : ", item.word);
+    fflush(stdin);
+    gets(item.uncommon);
+
+    // Get the description of the word
     item.description = (char*)malloc(MAX_SIZE_DESC * sizeof(char));
     printf("Masukkan deskripsi dari %s : ", item.word);
     fflush(stdin);
     gets(item.description);
+
+    // Insert the word item to item array
     kamus->data[kamus->length++] = item;
+
+    // Save the dictionary state
     write_data(kamus);
-    read_data(kamus);
+
+    // Refresh the dictionary state
+    refresh(kamus);
 }
 
+/**
+ * Function to sort word in dictionary A-Z
+ * @param Kamus* kamus
+ */
 void sortAscending(Kamus* kamus) {
     int i, j;
     for (i = 0; i < kamus->length - 1; i++) {
         for (j = i; j < kamus->length; j++) {
             if (strcmp(kamus->data[i].word, kamus->data[j].word) > 0) {
-                swapItem(&kamus->data[i], &kamus->data[j]);
-            }
-        }
-    }
-}
-
-void sortDescending(Kamus* kamus) {
-    int i, j;
-    for (i = 0; i < kamus->length - 1; i++) {
-        for (j = i; j < kamus->length; j++) {
-            if (strcmp(kamus->data[i].word, kamus->data[j].word) < 0) {
+                // Swap item if word[i] > word[j]
                 swapItem(&kamus->data[i], &kamus->data[j]);
             }
         }
@@ -168,8 +248,24 @@ void sortDescending(Kamus* kamus) {
 }
 
 /**
- * Function to swap 2 string element
- * @param : *element1, *element2
+ * Function to sort word in dictionary Z-A
+ * @param Kamus* kamus
+ */
+void sortDescending(Kamus* kamus) {
+    int i, j;
+    for (i = 0; i < kamus->length - 1; i++) {
+        for (j = i; j < kamus->length; j++) {
+            if (strcmp(kamus->data[i].word, kamus->data[j].word) < 0) {
+                // Swap item if word[i] < word[j]
+                swapItem(&kamus->data[i], &kamus->data[j]);
+            }
+        }
+    }
+}
+
+/**
+ * Function to swap Item in an array of Item
+ * @param *item_1 *item_2
  */
 void swapItem(Item* item_1, Item* item_2) {
     Item temp = *item_1;
@@ -179,17 +275,21 @@ void swapItem(Item* item_1, Item* item_2) {
 
 /**
  * Function to search word in dictionary
+ * @param Kamus* kamus
  */
 void searchWord(Kamus* kamus) {
+    // Get the word to search from the user
     char* search = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
     printf("\nSearch words: ");
     fflush(stdin);
     gets(search);
 
+    // Print all results that contain the searched word
     printf("-------RESULTS FOUNDED-------\n");
     for (int i = 0; i < kamus->length; i++) {
         char* ret;
         ret = strstr(kamus->data[i].word, search);
+        // Display word if the it contain the searched word
         if (ret != NULL) {
             display_item(kamus->data[i]);
         }
@@ -199,10 +299,16 @@ void searchWord(Kamus* kamus) {
     free(search);
 }
 
+/**
+ * Function to initialize dictionary
+ * @return Kamus
+ */
 Kamus initialize() {
     Kamus kamus;
+    // Allocated memory from item array
     kamus.data = (Item*)malloc(KAMUS_SIZE * sizeof(Item));
     kamus.length = 0;
+    // Read the data from saved dictionary
     read_data(&kamus);
     return kamus;
 }
