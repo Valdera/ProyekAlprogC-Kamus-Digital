2aeb09c8bc473b5d7c226b6568b26f56959c7825
Fauzan Valdera <f.valdera@yahoo.co.id>
2021-10-28 17:45:05 +0700

    Update main.c

--- main.c
+++ main.c
@@ -21,6 +21,10 @@ void display_all(Kamus* kamus);
 void write_data(Kamus* kamus);
 void read_data(Kamus* kamus);
 void add_item(Kamus* kamus);
+void sortAscending(Kamus* kamus);
+void sortDescending(Kamus* kamus);
+void searchWord(Kamus* kamus);
+void swapItem(Item* item_1, Item* item_2);
 Kamus initialize();
 
 int main() {
@@ -28,9 +32,12 @@ int main() {
     int loop = 1;
     int menu;
     while (loop) {
-        printf("\n\n-------KAMUS DIGITAL-------");
-        printf("\n1. Display all items in dictionary");
-        printf("\n2. Insert item in dictionary");
+        printf("-------KAMUS DIGITAL-------");
+        printf("\n1. Display all words in dictionary");
+        printf("\n2. Insert word in dictionary");
+        printf("\n3. Sort all words in dictionary A-Z ");
+        printf("\n4. Sort all words in dictionary Z-A ");
+        printf("\n5. Search word in dictionary");
         printf("\n6. Exit");
         printf("\nMasukkan menu pilihan : ");
         scanf("%d", &menu);
@@ -41,16 +48,29 @@ int main() {
             case 2:
                 add_item(&kamus);
                 break;
+            case 3:
+                sortAscending(&kamus);
+                display_all(&kamus);
+                break;
+            case 4:
+                sortDescending(&kamus);
+                display_all(&kamus);
+                break;
+            case 5:
+                searchWord(&kamus);
+                break;
             default:
                 loop = 0;
                 break;
         }
+        printf("\n");
     }
+    free(kamus.data);
     return 0;
 }
 
 void display_item(Item item) {
-    printf("\n----------------------");
+    printf("----------------------");
     printf("\nKata      : %s", item.word);
     printf("\nDeskripsi : %s", item.description);
     printf("----------------------");
@@ -58,8 +78,10 @@ void display_item(Item item) {
 
 void display_all(Kamus* kamus) {
     int i;
+    printf("\n-------LIST OF WORDS-------\n");
     for (i = 0; i < kamus->length; i++) {
         display_item(kamus->data[i]);
+        printf("\n");
     }
 }
 
@@ -123,6 +145,59 @@ void add_item(Kamus* kamus) {
     read_data(kamus);
 }
 
+void sortAscending(Kamus* kamus) {
+    int i, j;
+    for (i = 0; i < kamus->length - 1; i++) {
+        for (j = i; j < kamus->length; j++) {
+            if (strcmp(kamus->data[i].word, kamus->data[j].word) > 0) {
+                swapItem(&kamus->data[i], &kamus->data[j]);
+            }
+        }
+    }
+}
+
+void sortDescending(Kamus* kamus) {
+    int i, j;
+    for (i = 0; i < kamus->length - 1; i++) {
+        for (j = i; j < kamus->length; j++) {
+            if (strcmp(kamus->data[i].word, kamus->data[j].word) < 0) {
+                swapItem(&kamus->data[i], &kamus->data[j]);
+            }
+        }
+    }
+}
+
+/**
+ * Function to swap 2 string element
+ * @param : *element1, *element2
+ */
+void swapItem(Item* item_1, Item* item_2) {
+    Item temp = *item_1;
+    *item_1 = *item_2;
+    *item_2 = temp;
+}
+
+/**
+ * Function to search word in dictionary
+ */
+void searchWord(Kamus* kamus) {
+    char* search = (char*)malloc(MAX_SIZE_WORD * sizeof(char));
+    printf("\nSearch words: ");
+    fflush(stdin);
+    gets(search);
+
+    printf("-------RESULTS FOUNDED-------\n");
+    for (int i = 0; i < kamus->length; i++) {
+        char* ret;
+        ret = strstr(kamus->data[i].word, search);
+        if (ret != NULL) {
+            display_item(kamus->data[i]);
+        }
+    }
+
+    printf("\n");
+    free(search);
+}
 
 Kamus initialize() {
     Kamus kamus;
